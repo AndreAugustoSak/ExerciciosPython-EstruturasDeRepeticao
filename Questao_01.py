4cda2f720989e845a627b0cc35cfb833938b7563
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-06-18 14:25:41 -0300

    Update Questao_01.py

---
+++ Questao_01.py
@@ -0,0 +1,12 @@
+# Faça um programa que peça uma nota, entre zero e dez.
+# Mostre uma mensagem caso o valor seja inválido e
+# continue pedindo até que o usuário informe um valor válido.
+
+def estrutura_de_repeticao_01():
+    nota = float(input('Digite uma nota de 0 a 10: '))
+    if nota > 10 or nota < 0:
+        print('Número inválido!')
+        estrutura_de_repeticao_01()
+
+        
+estrutura_de_repeticao_01()
